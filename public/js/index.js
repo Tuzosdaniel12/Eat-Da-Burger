c12f18e0445ac9a173d94f55f63de506acb9d9d2
daniel <danielsoledad@gmail.com>
2021-01-11 00:07:38 -0800

    added all my post,put,read,delete routes to front end index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,38 @@
-const $burgerTextValue = $("#burger_textarea") 
+const $burgerTextValue = $("#burger_textarea")
+const $burgerList = $("#burger-list")
 
+//add burget to list
 const addBurger = async (e) =>{
     e.preventDefault();
 
     const burger = {
         burger_name : $("#add-burger").val()
     }
-    const res = await postBurger(burger).catch((err)=>{console.error(err)})
-    
-    console.log(res)
+    await postBurger(burger).catch((err)=>{console.error(err)})
 
     location.reload();
 }
 
-$burgerTextValue.submit(addBurger);
+//devour or remove burger
 
+async function removeOrDevourBurger(e){
+    e.preventDefault()
+    const burger = {id :$(this).data("id")};
+    let devoured;
+    
+    if($(this).data("devour")){
+        burger.devoured =false
+
+        await putBurger(burger).catch((err)=>{console.error(err)})
+
+        location.reload();
+    }else{
+        await deleteBurger(burger).catch((err)=>{console.error(err)})
+    }
+    console.log(burger)
+} 
+
+//post 
 const postBurger = (burger)=>{
     return $.ajax({
         url: "/api/burgers",
@@ -22,3 +40,27 @@ const postBurger = (burger)=>{
         method: "POST",
       });
 }
+
+//put 
+const putBurger = (burger)=>{
+    return $.ajax({
+        url: "/api/burgers"+ burger.id,
+        data: burger,
+        method: "PUT",
+      });
+}
+
+//delete 
+const deleteBurger = (burger)=>{
+    return $.ajax({
+        url: "/api/burgers"+ burger.id,
+        data: burger,
+        method: "DELETE",
+      });
+}
+
+//click events
+$burgerTextValue.submit(addBurger);
+$burgerList.on("click","[data-devour]",removeOrDevourBurger);
+
+
